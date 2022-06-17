d9a56f347b0f7c8980c662b8d93a7232c8968bd5
Hirisci <www.alan92@icloud.com>
2022-06-17 11:24:50 +0200

    fixato bug

---
+++ src/components/atom/ASocialIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <Fas class="icon" :icon="text"></Fas>
+</template>
+
+<script>
+export default {
+  name: "ASocialIcon",
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  height: 3rem;
+  aspect-ratio: 1/1;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+}
+</style>
